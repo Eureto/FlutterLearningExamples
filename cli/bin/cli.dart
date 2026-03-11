08c6e6a1e4bcd6cba0b052def172af7022829582
Eureto <matek891@gmail.com>
2026-03-11 13:43:10 +0100

    Fetching articles from wikipedia using http package

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,36 +1,55 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = "1.1.1";
 void main(List<String> arguments) {
-
-  if(arguments.isEmpty || arguments.first == "help"){
+  if (arguments.isEmpty || arguments.first == "help") {
     printUserInformation();
-  }else if(arguments.first == "--version" || arguments.first == "-V"){
+  } else if (arguments.first == "--version" || arguments.first == "-V") {
     print("Version: $version");
-  }else if(arguments.first == "search")
-  {
-    final inputList = arguments.length > 1 ? arguments.sublist(1): null;
+  } else if (arguments.first == "search") {
+    final inputList = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputList);
-  }else{
+  } else {
     printUserInformation();
   }
 }
 
-void printUserInformation(){
-  print("this is test program");
+void printUserInformation() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
 }
 
-void searchWikipedia(List<String>? arguments){
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
 
-  if (arguments == null || arguments.isEmpty){
+  if (arguments == null || arguments.isEmpty) {
     print("Please provide titles:");
     articleTitle = stdin.readLineSync() ?? "";
-  }else{
+    if (articleTitle == null || articleTitle.isEmpty) {
+      print("You must provide article title, exiting funciton");
+      return;
+    }
+  } else {
     articleTitle = arguments.join(" ");
   }
-  
+
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return response.body;
   }
+  return 'Error: Failed to fetch article, Status code: ${response.statusCode}';
+}
