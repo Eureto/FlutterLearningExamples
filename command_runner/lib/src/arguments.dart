6ef4d8ee7d15f00e508c17a8a6c3f2d4550f16cf
Eureto <matek891@gmail.com>
2026-03-13 00:29:33 +0100

    crating structure of classes for arguments in cli

---
+++ command_runner/lib/src/arguments.dart
@@ -0,0 +1,70 @@
+import '../command_runner.dart';
+import 'dart:collection';
+
+enum OptionType { flat, option }
+
+abstract class Argument {
+  String get name;
+  String? get help;
+
+  Object? get defaultValue;
+  String? get valueHelp;
+
+  String get usage;
+}
+
+class Option extends Argument {
+  Option(
+    this.name, {
+    required this.type,
+    this.help,
+    this.abbr,
+    this.defaultValue,
+    this.valueHelp,
+  });
+
+  @override
+  final String name;
+
+  final OptionType type;
+
+  @override
+  final String? help;
+
+  final String? abbr;
+
+  @override
+  final Object? defaultValue;
+
+  @override
+  final String? valueHelp;
+
+  @override
+  String get usage {
+    if (abbr == null) {
+      return '--$abbr,--$name: $help';
+    }
+    return '--$name: $help';
+  }
+}
+
+abstract class Command extends Argument {
+  @override
+  String get name;
+  String get description;
+  bool get requiresArgument => false;
+  late CommandRunner runner;
+  @override
+  String? help;
+  @override
+  String? defaultValue;
+  @override
+  String? valueHelp;
+
+  final List<Option> _options = [];
+
+  UnmodifiableSetView<Option> get options =>
+      UnmodifiableSetView(_options.toSet());
+
+  //To make adding options easier, we'll provide two helper methods, addFlag and addOption
+}
